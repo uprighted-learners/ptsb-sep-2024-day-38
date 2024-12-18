2ad4f9f8a27079f930112a432a1f6716e6f0c3bf
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-12-18 17:32:50 -0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+const PORT = 8080;
 
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+// api health
 app.get('/', (req, res) => {
     res.send('Hello from the server');
 })
 
-app.listen(8080)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
